1a6c1ebd1972e568c50516868d33df26353f1d55
Archit Bansal <archit.bansal@flipkart.com>
2018-06-15 08:10:23 +0530

    removing foreign key in entity for user

--- server/dbclient/db/migrations/20180614233323_ModifyColumns.sql
+++ server/dbclient/db/migrations/20180614233323_ModifyColumns.sql
@@ -3,6 +3,8 @@
 
 alter table requests modify column `owner_comment` text not null default '', modify column `requester_comment` text not null default '';
 alter table entity modify column price int(11) not null default 0, modify `borrower` varchar(50) DEFAULT NULL;
+alter table entity drop foreign key entity_borrower_user_user_id;
+alter table entity drop foreign key entity_user_user_id;
 
 -- +goose Down
 -- SQL section 'Down' is executed when this migration is rolled back
